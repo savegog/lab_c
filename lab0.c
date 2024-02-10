782956a972bb3ba00b836774ac326f4e49c5f333
savegog <159597888+savegog@users.noreply.github.com>
2024-02-10 13:13:14 +1000

    Update lab0.c

--- lab0.c
+++ lab0.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
-{
-    printf("Hello World");
-
+{ double a = 3, b = 4, c = -2, D, x1,x2;
+    D = b*b - 4*a*c;
+    x1 = (-b + sqrt(D))/(2*a);
+    x2 = (-b-sqrt(D))/(2*a);
+    printf("d= %lf \n", D);
+    printf("x1=%lf, x2=%lf", x1,x2);
+    
+    
     return 0;
 }
