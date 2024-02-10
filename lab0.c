f412353804dca159468b6b54ca5bafc863d5f556
savegog <159597888+savegog@users.noreply.github.com>
2024-02-10 13:23:49 +1000

    Update lab0.c

--- lab0.c
+++ lab0.c
@@ -2,11 +2,27 @@
 #include <math.h>
 int main()
 { double a = 3, b = 4, c = -2, D, x1,x2;
+printf("Vvedite a");
+scanf("%lf", &a);
+printf("Vvedite b");
+scanf("%lf", &b);
+printf("Vvedite c");
+scanf("%lf", &c);
     D = b*b - 4*a*c;
+if (D>0) {
+    
     x1 = (-b + sqrt(D))/(2*a);
     x2 = (-b-sqrt(D))/(2*a);
-    printf("d= %lf \n", D);
     printf("x1=%lf, x2=%lf", x1,x2);
+    
+}
+if (D<0) {
+    printf("Kornei net");
+    
+}
+if (D == 0) {
+    printf("x1=%lf", x1);
+}
     
     
     return 0;
