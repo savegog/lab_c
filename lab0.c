6bd48ede6ffe920f3e7df2ab197087211be149f5
savegog <159597888+savegog@users.noreply.github.com>
2024-02-17 00:38:59 +1000

    Update lab1.c

--- lab0.c
+++ lab0.c
@@ -2,13 +2,14 @@
 #include <math.h>
 int main()
 { double a = 3, b = 4, c = -2, D, x1,x2;
+
 printf("Vvedite a");
 scanf("%lf", &a);
 printf("Vvedite b");
 scanf("%lf", &b);
 printf("Vvedite c");
 scanf("%lf", &c);
-    D = b*b - 4*a*c;
+    D = pow(b,2) - 4*a*c;
 if (D>0) {
     
     x1 = (-b + sqrt(D))/(2*a);
