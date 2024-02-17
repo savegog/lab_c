b513b5193e9b446c84f50725f6736191aeb8ff36
savegog <159597888+savegog@users.noreply.github.com>
2024-02-17 12:53:17 +1000

    Update lab0.c

--- lab0.c
+++ lab0.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 int main()
-{ double a = 3, b = 4, c = -2, D, x1,x2;
+{     double a , b , c , D, x1,x2;
+    int answer;
+    printf("Programma prodolzaetsya? (1/0):");
+    scanf("%d", &answer );
+    while (answer == 1){
+        
 
-printf("Vvedite a");
-scanf("%lf", &a);
-printf("Vvedite b");
-scanf("%lf", &b);
-printf("Vvedite c");
-scanf("%lf", &c);
-    D = pow(b,2) - 4*a*c;
-if (D>0) {
+        printf("Vvedite a\n");
+        scanf("%lf", &a);
+        printf("Vvedite b\n");
+        scanf("%lf", &b);
+        printf("Vvedite c\n");
+        scanf("%lf", &c);
+        D = pow(b,2) - 4*a*c;
+        if (D>0) {
     
-    x1 = (-b + sqrt(D))/(2*a);
-    x2 = (-b-sqrt(D))/(2*a);
-    printf("x1=%lf, x2=%lf", x1,x2);
+            x1 = (-b + sqrt(D))/(2*a);
+            x2 = (-b-sqrt(D))/(2*a);
+            printf("x1=%lf, x2=%lf", x1,x2);
+            printf("\n");
+}
+        if (D<0) {
+            printf("Kornei net\n");
     
 }
-if (D<0) {
-    printf("Kornei net");
-    
+        if (D == 0) {
+            printf("x1=%lf", x1, "\n");
 }
-if (D == 0) {
-    printf("x1=%lf", x1);
-}
-    
+    }
+
     
     return 0;
 }
